9dcd6ca4f9f226c64a228ff6805693f1b798a010
matu.dev <mati.a.gimenez2002@gmail.com>
2025-09-11 10:48:45 -0300

    Calase 22

---
+++ Clase-22-React/src/Components/ColorCard/ColorCard.jsx
@@ -0,0 +1,23 @@
+//DIRECCION DE LA PAGINA: https://colorhunt.co/
+
+/* 
+Crear un componente llamado ColorCard que reciba las props:
+- likes: number, cantidad de likes
+- date: string, la fecha de subida de esa tarjeta, EJ: "yesterday", '13 hours'
+- colors: array string, los 4 colores que tendra la tarjeta de color,EJ: ['#708993','#139ed5ff', '#938970ff', '#937090ff']
+
+Ejemplo de uso:
+
+<ColorCard 
+    likes={63} 
+    date='Yesterday' 
+    colors={[
+    '#708993',
+    '#139ed5ff', 
+    '#938970ff', 
+    '#937090ff'
+    ]}
+/>
+
+No hace falta fidelidad visual, pero si se deben ver los colores de fondo
+*/
